610d34812dfc5dc673c24aa7c8af63e75a1c1270
Mark Baggett <markpbaggett@gmail.com>
2025-07-15 07:11:20 -0500

    Init navplace in annotations.

---
+++ manifests/tests/navplace-in-annotations.py
@@ -0,0 +1,504 @@
+{
+  "@context": [
+    "http://iiif.io/api/extension/navplace/context.json",
+    "http://iiif.io/api/presentation/3/context.json"
+  ],
+  "id": "https://markpbaggett.github.io/static_iiif/manifests/tests/navplace-in-annotations.json",
+  "type": "Manifest",
+  "label": {
+    "en": [
+      "Black Bear; The Great Smoky Mountains National Park"
+    ]
+  },
+  "summary": {
+    "en": [
+      ".",
+      "Postcard with handwritten note sent from Knoxville to Miss Virginia Bogart, Loudon, Tennessee on March 2, 1944 for a postage of 1 cent.",
+      ".",
+      "The hardwood forest of America, and probably of the entire world, originated in the Great Smoky Mountains, where remains the nation's largest body of virgin hardwood forest, and the world's greatest variety of trees, flowering shrubs and wild flowers."
+    ]
+  },
+  "metadata": [
+    {
+      "label": {
+        "en": [
+          "Date"
+        ]
+      },
+      "value": {
+        "en": [
+          "1944-03-02"
+        ]
+      }
+    },
+    {
+      "label": {
+        "en": [
+          "Format"
+        ]
+      },
+      "value": {
+        "en": [
+          "postcards"
+        ]
+      }
+    },
+    {
+      "label": {
+        "en": [
+          "Subject"
+        ]
+      },
+      "value": {
+        "en": [
+          "Black bear",
+          "Black bears"
+        ]
+      }
+    },
+    {
+      "label": {
+        "en": [
+          "Place"
+        ]
+      },
+      "value": {
+        "en": [
+          "Great Smoky Mountains (N.C. and Tenn.)"
+        ]
+      }
+    },
+    {
+      "label": {
+        "en": [
+          "Description"
+        ]
+      },
+      "value": {
+        "en": [
+          ".",
+          "Postcard with handwritten note sent from Knoxville to Miss Virginia Bogart, Loudon, Tennessee on March 2, 1944 for a postage of 1 cent.",
+          ".",
+          "The hardwood forest of America, and probably of the entire world, originated in the Great Smoky Mountains, where remains the nation's largest body of virgin hardwood forest, and the world's greatest variety of trees, flowering shrubs and wild flowers."
+        ]
+      }
+    },
+    {
+      "label": {
+        "en": [
+          "Language"
+        ]
+      },
+      "value": {
+        "en": [
+          "eng"
+        ]
+      }
+    },
+    {
+      "label": {
+        "en": [
+          "Provided by"
+        ]
+      },
+      "value": {
+        "en": [
+          "University of Tennessee, Knoxville. Libraries"
+        ]
+      }
+    },
+    {
+      "label": {
+        "en": [
+          "Creator"
+        ]
+      },
+      "value": {
+        "en": [
+          "Standard News Agency (Knoxville, Tenn.)",
+          "unknown"
+        ]
+      }
+    }
+  ],
+  "rights": "http://rightsstatements.org/vocab/CNE/1.0/",
+  "requiredStatement": {
+    "label": {
+      "en": [
+        "Rights"
+      ]
+    },
+    "value": {
+      "en": [
+        "\u003Cspan\u003E\u003Ca href=\"http://rightsstatements.org/vocab/CNE/1.0/\"\u003E\u003Cimg src=\"https://rightsstatements.org/files/buttons/CNE.dark-white-interior-blue-type.svg\"/\u003E\u003C/a\u003E\u003C/span\u003E\u003Cbr/\u003E\u003Cspan\u003E\u003Ca href=\"http://rightsstatements.org/vocab/CNE/1.0/\"\u003ECopyright Not Evaluated\u003C/a\u003E:  The copyright and related rights status of this Item has not been evaluated. Please refer to the organization that has made the Item available for more information. You are free to use this Item in any way that is permitted by the copyright and related rights legislation that applies to your use.\u003C/span\u003E\u003Cbr/\u003E"
+      ]
+    }
+  },
+  "navDate": "1944-03-02T07:47:20Z",
+  "navPlace": {
+    "id": "https://digital.lib.utk.edu/notdereferenceable/featurecollection//1",
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "id": "https://digital.lib.utk.edu/notdereferenceable/feature/1",
+        "type": "Feature",
+        "properties": {
+          "label": {
+            "en": [
+              "Black Bear; The Great Smoky Mountains National Park -- Great Smoky Mountains (N.C. and Tenn.)"
+            ]
+          }
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-83.50822, 35.58343]
+        }
+      }
+    ]
+  },
+  "provider": [
+    {
+      "id": "https://www.lib.utk.edu/about/",
+      "type": "Agent",
+      "label": {
+        "en": [
+          "University of Tennessee, Knoxville. Libraries"
+        ]
+      },
+      "homepage": [
+        {
+          "id": "https://www.lib.utk.edu/",
+          "type": "Text",
+          "label": {
+            "en": [
+              "University of Tennessee Libraries Homepage"
+            ]
+          },
+          "format": "text/html"
+        }
+      ],
+      "logo": [
+        {
+          "id": "https://utkdigitalinitiatives.github.io/iiif-level-0/ut_libraries_centered/full/full/0/default.jpg",
+          "type": "Image",
+          "format": "image/jpeg",
+          "width": 200,
+          "height": 200
+        }
+      ]
+    }
+  ],
+  "thumbnail": [
+    {
+      "id": "https://digital.lib.utk.edu/collections/islandora/object/pcard00%3A100201/datastream/TN",
+      "width": 200,
+      "height": 200,
+      "type": "Image",
+      "format": "image/jpeg"
+    }
+  ],
+  "items": [
+    {
+      "id": "https://digital.lib.utk.edu/notdereferenceable/assemble/manifest/pcard00/100201/canvas/0",
+      "type": "Canvas",
+      "label": {
+        "none": [
+          "Black Bear; The Great Smoky Mountains National Park"
+        ]
+      },
+      "width": 2284,
+      "height": 1457,
+      "thumbnail": [
+        {
+          "id": "https://digital.lib.utk.edu/iiif/2/collections~islandora~object~100201:8~datastream~TN/full/full/0/default.jpg",
+          "width": 200,
+          "height": 200,
+          "type": "Image",
+          "format": "image/jpeg",
+          "service": [
+            {
+              "@id": "https://digital.lib.utk.edu/iiif/2/collections~islandora~object~100201:8~datastream~TN",
+              "@type": "http://iiif.io/api/image/2/context.json",
+              "profile": "http://iiif.io/api/image/2/level2.json"
+            }
+          ]
+        }
+      ],
+      "items": [
+        {
+          "id": "https://digital.lib.utk.edu/notdereferenceable/assemble/manifest/pcard00/100201/canvas/0/page/100201:8",
+          "type": "AnnotationPage",
+          "items": [
+            {
+              "id": "https://digital.lib.utk.edu/notdereferenceable/assemble/manifest/pcard00/100201/canvas/0/page/100201:8/68763fcb0d04a",
+              "type": "Annotation",
+              "motivation": "painting",
+              "body": {
+                "id": "https://digital.lib.utk.edu/iiif/2/collections~islandora~object~100201:8~datastream~JP2/full/full/0/default.jpg",
+                "type": "Image",
+                "width": 2284,
+                "height": 1457,
+                "format": "image/jpeg",
+                "service": [
+                  {
+                    "@id": "https://digital.lib.utk.edu/iiif/2/collections~islandora~object~100201:8~datastream~JP2",
+                    "@type": "http://iiif.io/api/image/2/context.json",
+                    "profile": "http://iiif.io/api/image/2/level2.json"
+                  }
+                ]
+              },
+              "target": "https://digital.lib.utk.edu/notdereferenceable/assemble/manifest/pcard00/100201/canvas/0"
+            }
+          ]
+        }
+      ],
+      "summary": {
+        "en": [
+          "Postcard with handwritten note sent from Knoxville to Miss Virginia Bogart, Loudon, Tennessee on March 2, 1944 for a postage of 1 cent.",
+          "The hardwood forest of America, and probably of the entire world, originated in the Great Smoky Mountains, where remains the nation's largest body of virgin hardwood forest, and the world's greatest variety of trees, flowering shrubs and wild flowers."
+        ]
+      },
+      "metadata": [
+        {
+          "label": {
+            "en": [
+              "Canvas Label"
+            ]
+          },
+          "value": {
+            "en": [
+              "Black Bear; The Great Smoky Mountains National Park"
+            ]
+          }
+        }
+      ],
+      "rights": "http://rightsstatements.org/vocab/CNE/1.0/",
+      "requiredStatement": {
+        "label": {
+          "en": [
+            "Rights"
+          ]
+        },
+        "value": {
+          "en": [
+            "\u003Cspan\u003E\u003Ca href=\"http://rightsstatements.org/vocab/CNE/1.0/\"\u003E\u003Cimg src=\"https://rightsstatements.org/files/buttons/CNE.dark-white-interior-blue-type.svg\"/\u003E\u003C/a\u003E\u003C/span\u003E\u003Cbr/\u003E\u003Cspan\u003E\u003Ca href=\"http://rightsstatements.org/vocab/CNE/1.0/\"\u003ECopyright Not Evaluated\u003C/a\u003E:  The copyright and related rights status of this Item has not been evaluated. Please refer to the organization that has made the Item available for more information. You are free to use this Item in any way that is permitted by the copyright and related rights legislation that applies to your use.\u003C/span\u003E\u003Cbr/\u003E"
+          ]
+        }
+      }
+    },
+    {
+      "id": "https://digital.lib.utk.edu/notdereferenceable/assemble/manifest/pcard00/100201/canvas/1",
+      "type": "Canvas",
+      "label": {
+        "none": [
+          "Black Bear; The Great Smoky Mountains National Park"
+        ]
+      },
+      "width": 2286,
+      "height": 1456,
+      "thumbnail": [
+        {
+          "id": "https://digital.lib.utk.edu/iiif/2/collections~islandora~object~100201:7~datastream~TN/full/full/0/default.jpg",
+          "width": 200,
+          "height": 200,
+          "type": "Image",
+          "format": "image/jpeg",
+          "service": [
+            {
+              "@id": "https://digital.lib.utk.edu/iiif/2/collections~islandora~object~100201:7~datastream~TN",
+              "@type": "http://iiif.io/api/image/2/context.json",
+              "profile": "http://iiif.io/api/image/2/level2.json"
+            }
+          ]
+        }
+      ],
+      "items": [
+        {
+          "id": "https://digital.lib.utk.edu/notdereferenceable/assemble/manifest/pcard00/100201/canvas/1/page/100201:7",
+          "type": "AnnotationPage",
+          "items": [
+            {
+              "id": "https://digital.lib.utk.edu/notdereferenceable/assemble/manifest/pcard00/100201/canvas/1/page/100201:7/68763fcb9160b",
+              "type": "Annotation",
+              "motivation": "painting",
+              "body": {
+                "id": "https://digital.lib.utk.edu/iiif/2/collections~islandora~object~100201:7~datastream~JP2/full/full/0/default.jpg",
+                "type": "Image",
+                "width": 2286,
+                "height": 1456,
+                "format": "image/jpeg",
+                "service": [
+                  {
+                    "@id": "https://digital.lib.utk.edu/iiif/2/collections~islandora~object~100201:7~datastream~JP2",
+                    "@type": "http://iiif.io/api/image/2/context.json",
+                    "profile": "http://iiif.io/api/image/2/level2.json"
+                  }
+                ]
+              },
+              "target": "https://digital.lib.utk.edu/notdereferenceable/assemble/manifest/pcard00/100201/canvas/1"
+            }
+          ]
+        }
+      ],
+      "annotations": [
+        {
+          "id": "https://digital.lib.utk.edu/notdereferenceable/assemble/manifest/pcard00/100201/canvas/1/annotation/1",
+          "type": "AnnotationPage",
+          "items": [
+            {
+              "id": "https://digital.lib.utk.edu/notdereferenceable/assemble/manifest/pcard00/100201/canvas/1/annotation/1/idloc",
+              "type": "Annotation",
+              "motivation": "tagging",
+              "body": [
+                {
+                  "type": "SpecificResource",
+                  "source": "https://sws.geonames.org/12213638"
+                },
+                {
+                  "type": "TextualBody",
+                  "value": "Knoxville, TN",
+                  "format": "text/plain",
+                  "language": "en"
+                }
+              ],
+              "target": "https://digital.lib.utk.edu/notdereferenceable/assemble/manifest/pcard00/100201/canvas/1#xywh=793,195,431,408",
+              "navPlace": {
+                "id": "https://digital.lib.utk.edu/notdereferenceable/featurecollection/1/anno/1",
+                "type": "FeatureCollection",
+                "features": [
+                  {
+                    "id": "https://digital.lib.utk.edu/notdereferenceable/feature/1/anno/1",
+                    "type": "Feature",
+                    "properties": {
+                    "label": {
+                        "en": [
+                        "Knoxville, TN"
+                        ]
+                      }
+                    },
+                    "geometry": {
+                    "type": "Point",
+                    "coordinates": [-84.19438, 36.02374]
+                    }
+                  }
+                ]
+              },
+            }
+          ]
+        },
+        {
+          "id": "https://digital.lib.utk.edu/notdereferenceable/assemble/manifest/pcard00/100201/canvas/1/annotation/2",
+          "type": "AnnotationPage",
+          "items": [
+            {
+              "id": "https://digital.lib.utk.edu/notdereferenceable/assemble/manifest/pcard00/100201/canvas/1/annotation/2/idloc",
+              "type": "Annotation",
+              "motivation": "tagging",
+              "body": [
+                {
+                  "type": "SpecificResource",
+                  "source": "https://sws.geonames.org/4638278"
+                },
+                {
+                  "type": "TextualBody",
+                  "value": "Loudon, TN",
+                  "format": "text/plain",
+                  "language": "en"
+                }
+              ],
+              "target": "https://digital.lib.utk.edu/notdereferenceable/assemble/manifest/pcard00/100201/canvas/1#xywh=1122,1018,1119,384",
+              "navPlace": {
+                "id": "https://digital.lib.utk.edu/notdereferenceable/featurecollection/1/anno/2",
+                "type": "FeatureCollection",
+                "features": [
+                  {
+                    "id": "https://digital.lib.utk.edu/notdereferenceable/feature/1/anno/2",
+                    "type": "Feature",
+                    "properties": {
+                    "label": {
+                        "en": [
+                        "Loudon, TN"
+                        ]
+                      }
+                    },
+                    "geometry": {
+                    "type": "Point",
+                    "coordinates": [-84.33381, 35.73285]
+                    }
+                  }
+                ]
+              },
+            }
+          ]
+        }
+      ],
+      "summary": {
+        "en": [
+          "Postcard with handwritten note sent from Knoxville to Miss Virginia Bogart, Loudon, Tennessee on March 2, 1944 for a postage of 1 cent.",
+          "The hardwood forest of America, and probably of the entire world, originated in the Great Smoky Mountains, where remains the nation's largest body of virgin hardwood forest, and the world's greatest variety of trees, flowering shrubs and wild flowers."
+        ]
+      },
+      "metadata": [
+        {
+          "label": {
+            "en": [
+              "Canvas Label"
+            ]
+          },
+          "value": {
+            "en": [
+              "Black Bear; The Great Smoky Mountains National Park"
+            ]
+          }
+        }
+      ],
+      "rights": "http://rightsstatements.org/vocab/CNE/1.0/",
+      "requiredStatement": {
+        "label": {
+          "en": [
+            "Rights"
+          ]
+        },
+        "value": {
+          "en": [
+            "\u003Cspan\u003E\u003Ca href=\"http://rightsstatements.org/vocab/CNE/1.0/\"\u003E\u003Cimg src=\"https://rightsstatements.org/files/buttons/CNE.dark-white-interior-blue-type.svg\"/\u003E\u003C/a\u003E\u003C/span\u003E\u003Cbr/\u003E\u003Cspan\u003E\u003Ca href=\"http://rightsstatements.org/vocab/CNE/1.0/\"\u003ECopyright Not Evaluated\u003C/a\u003E:  The copyright and related rights status of this Item has not been evaluated. Please refer to the organization that has made the Item available for more information. You are free to use this Item in any way that is permitted by the copyright and related rights legislation that applies to your use.\u003C/span\u003E\u003Cbr/\u003E"
+          ]
+        }
+      }
+    }
+  ],
+  "seeAlso": [
+    {
+      "id": "https://digital.lib.utk.edu/collections/islandora/object/pcard00%3A100201/datastream/MODS",
+      "type": "Dataset",
+      "label": {
+        "en": [
+          "Bibliographic Description in MODS"
+        ]
+      },
+      "format": "application/xml",
+      "profile": "http://www.loc.gov/standards/mods/v3/mods-3-5.xsd"
+    }
+  ],
+  "partOf": [
+    {
+      "id": "https://digital.lib.utk.edu/assemble/collection/gsmrc/pcard00",
+      "type": "Collection"
+    }
+  ],
+  "homepage": [
+    {
+      "id": "https://digital.lib.utk.edu/collections/islandora/object/pcard00%3A100201",
+      "label": {
+        "en": [
+          "Black Bear; The Great Smoky Mountains National Park"
+        ]
+      },
+      "type": "Text",
+      "format": "text/html"
+    }
+  ],
+  "behavior": [
+    "individuals"
+  ]
+}
